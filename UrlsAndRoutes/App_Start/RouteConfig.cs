04dd507b700f986631190a075ea12228e3ea0f7c
Vu Duy Thanh <dienbien001@gmail.com>
2017-11-10 16:36:09 +0700

    After completed

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -9,7 +9,13 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.RouteExistingFiles = true;
             routes.MapMvcAttributeRoutes();
+
+            routes.IgnoreRoute("Content/{filename}.html");
+
+            //routes.MapRoute("DiskFile", "Content/StaticContent.html",
+            //    new { controller = "Customer", action = "List" });
 
             routes.Add(new Route("SayHello", new CustomRouteHandler()));
 
@@ -17,8 +23,10 @@ namespace UrlsAndRoutes
                     "~/articles/Windows_3.1_Overview.html",
                     "~/old/.NET_1.0_Class_Library"));
 
-            routes.MapRoute("MyRoute", "{controller}/{action}/{id}");
-            routes.MapRoute("MyOtherRoute", "App/{action}", new { controller = "Home" });
+            routes.MapRoute("MyRoute", "{controller}/{action}",
+                new[] { "UrlsAndRoutes.Controllers" });
+            routes.MapRoute("MyOtherRoute", "App/{action}", new { controller = "Home" },
+                new[] { "UrlsAndRoutes.Controllers" });
         }
     }
 }
